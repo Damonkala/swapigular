c81ec915eeb6da7e42d78417f51d6dec5d6277dc
Damon Kalaboukis <damonkala@yahoo.com>
2015-11-23 21:45:42 -0800

    Same thing, but better looking

--- app.js
+++ app.js
@@ -3,29 +3,25 @@ myapp.controller("ResidentCtrl", function($scope, $http, $stateParams) {
 		$http.get("http://swapi.co/api/people/" + $stateParams.id + "/?format=json").then(resp => {
 				$scope.character = resp.data;
 				sessionStorage.setItem("resident " + $stateParams.id, resp.data.name);
-				// this.text(sessionStorage.getItem(resp.data.name));
 		});
 })
 myapp.controller("PlanetCtrl", function($scope, $http) {
 		$scope.planets = [];
 		$http.get("http://swapi.co/api/planets/?format=json").then(resp => {
 				$scope.planets = resp.data.results.map(planet => {
+					sessionStorage.setItem("planet " + planet.id, planet.name);
 						planet.residents = planet.residents.map(resident => {
 								var resident = { url: resident };
 								resident.id = resident.url.match(/\d+/)[0];
 							if(sessionStorage.getItem("resident " + resident.id)){
-								console.log("ALREADY HERE !!!!!")
-								resident =  sessionStorage.getItem("resident " + resident.id);
-								console.log(sessionStorage.getItem("resident " + resident.id));
+								resident.name =  sessionStorage.getItem("resident " + resident.id);
+								resident.url = "";
 								return resident;
 							}
 								return resident;
-
-
 						});
 						return planet;
 				});
-
 		}).catch(error => console.error(error.status));
 });
 myapp.config(function($stateProvider, $urlRouterProvider){
